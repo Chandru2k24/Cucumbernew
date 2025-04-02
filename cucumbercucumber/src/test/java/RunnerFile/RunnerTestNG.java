2ff6e19cbfa2ab79525805420a52fe2ea6e8b73c
ChandruJ <2k21it08@kiot.ac.in>
2025-04-02 10:37:04 +0530

    commit

--- cucumbercucumber/src/test/java/RunnerFile/RunnerTestNG.java
+++ cucumbercucumber/src/test/java/RunnerFile/RunnerTestNG.java
@@ -2,7 +2,7 @@ package RunnerFile;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-@CucumberOptions(plugin = {"pretty","html:target/cucumber-reports/Cucumber.html"}, features="src/test/resources/com/feature/SaucedemoLogin.feature",
+@CucumberOptions(plugin = {"pretty","json:target/cucumber-reports/Cucumber.json"}, features="src/test/resources/com/feature/SaucedemoLogin.feature",
 glue="step")
 public class RunnerTestNG extends AbstractTestNGCucumberTests {
 
