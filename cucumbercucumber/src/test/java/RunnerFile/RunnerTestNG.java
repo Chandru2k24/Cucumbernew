a43260e37569d524cb38f1498fe320c0773c3e3f
ChandruJ <2k21it08@kiot.ac.in>
2025-04-02 12:34:48 +0530

    commited on 12:34

--- cucumbercucumber/src/test/java/RunnerFile/RunnerTestNG.java
+++ cucumbercucumber/src/test/java/RunnerFile/RunnerTestNG.java
@@ -3,7 +3,7 @@ package RunnerFile;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(plugin = {"pretty","json:target/cucumber-reports/Cucumber.json"}, features="src/test/resources/com/feature/SaucedemoLogin.feature",
-glue="step",tags=("@ValidCredentials or  @InvalidCredentials(username)"))
+glue="step",tags=("not @valid"))
 public class RunnerTestNG extends AbstractTestNGCucumberTests {
 
 	public static void main(String[] args) {
